3cd9043928dc206007c133f66113047532694879
cheslev <cheslev01@gmail.com>
2022-01-19 22:25:25 +0300

    First Three Labaratories 19.01.2022

---
+++ Lab1.Ex2/Lab1.Ex2/nForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab1.Ex2
+{
+    public partial class nForm : Lab1.Ex2.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lab1.Ex5.2/Lab1.Ex5.2/StartForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1.Ex5._2
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChildForm f2 = new ChildForm();
+            f2.ShowDialog();
+        }
+
+        private void StartForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
